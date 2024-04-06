042e1ddbd9e39a37da438468e32675e72fe26214
Fernando Herrera <u.gomezherrera@gmail.com>
2024-04-06 09:24:29 -0600

    Agregar archivos de proyecto.

---
+++ L01P02_2021GH601/Models/facultad.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P02_2021GH601.Models
+{
+    public class facultad
+    {
+        [Key]
+        public int id { get; set; }
+        public string? facultades { get; set; }
+    }
+}
---
+++ L01P02_2021GH601/Models/notasContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2021GH601.Models
+{
+    public class notasContext : DbContext
+    {
+        public notasContext(DbContextOptions<notasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<alumnos> alumnos { get; set; }
+        public DbSet<departamentos> departamentos { get; set; }
+        public DbSet<facultad> facultad { get; set; }
+        public DbSet<materias> materias { get; set; }
+    }
+}
